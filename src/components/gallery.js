90ab2f3b94b42eab95d691b2447ba47699789c08
Hugo Fonseca <imhugofonseca@gmail.com>
2017-07-30 03:55:12 +0100

    More improvements

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -73,7 +73,7 @@ const renderPost = ({ id, slug, featured_media_url, title, i }) => (
     <Item key={id} to={`/post/${slug}`} className={`animated fadeIn`} style={{
         animationDelay: `${300 * i}ms`
     }}>
-        <Image src={`${featured_media_url}?quality=60&strip=info`} alt={title}></Image>
+        <Image src={`${featured_media_url}?quality=60&strip=info&w=900`} alt={title}></Image>
         <Hover className={'hover'}>
             <Title dangerouslySetInnerHTML={{ __html: title }} />
         </Hover>
